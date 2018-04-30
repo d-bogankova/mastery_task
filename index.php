e2acc5572ba498e8c855f29f3c34c0e0a3e17a43
d-bogankova <d.bogankova@godeltech.com>
2018-04-30 17:07:17 +0300

    Error output have bin optimized

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+function isUsernameValid ($username){
+    if (preg_match('/^\p{L}[\p{L}\p{N}\-\s]+$/u', $username)){
+        return true;
+    }
+    return false;
+}
+
+function isNaturalNum ($number){
+    if (intval($number) < 1){
+        return false;
+    }
+    return true;
+}
+
 function getNewFile ($username, $taskNumber, $months) {
     $username = preg_replace('#[^\p{L}\p{N}\s-]+#u','',$username);
     $taskNumber = intval($taskNumber);
@@ -16,58 +30,49 @@ function getNewFile ($username, $taskNumber, $months) {
 if (isset($argv[0])) {
     if (!isset($argv[1])) {
         die ("ERROR: Username not specified");
-    } elseif (is_numeric($argv[1])==true) {
+    } elseif (isUsernameValid($argv[1])==false) {
         die('ERROR: Username is not string ');
     }
     if (!isset($argv[2])) {
         die ("ERROR: Task number not specified");
-    } elseif (is_numeric($argv[2])==false) {
+    } elseif (isNaturalNum($argv[2])==false) {
         die('ERROR: Task number should be indicated as a numerical value ');
     }
     if (!isset($argv[3])) {
         die ("ERROR: Month number not specified");
-    } elseif (is_numeric($argv[3])==false) {
+    } elseif (isNaturalNum($argv[3])==false) {
         die('ERROR: Month number should be indicated as a numerical value ');
     }
     echo  getNewFile($argv[1],$argv[2],$argv[3]);
 } else {
-    if (isset($_POST) and $_SERVER["REQUEST_METHOD"] == "POST"){
+    if (!empty($_POST) and $_SERVER["REQUEST_METHOD"] == "POST"){
         $newFile = getNewFile($_POST['username'], $_POST['taskNumber'], $_POST['months']);
     }
 }
-//Error output browser
-if (!empty($_POST)) {
-    if (isset($_POST['submit']) && !empty($_POST['username']) ) {
-        $username = $_POST['username'];
-    } else {
-        die('Username is empty');
-    }
-    if (is_numeric($_POST['username'])===false) {
-        $username = $_POST['username'];
-    } else {
-        die('Username is not string');
-    }
-    if (isset($_POST['submit']) && !empty($_POST['taskNumber'])) {
-        $taskNumber = $_POST['taskNumber'];
-    } else {
-        die('Task Number is empty');
-    }
-    if (is_numeric($_POST['taskNumber'])){
-        $taskNumber = $_POST['taskNumber'];
-    } else {
-        die('Task Number should be indicated as a numerical value');
-    }
 
-    if (isset($_POST['submit']) && !empty($_POST['months'])) {
-        $months = $_POST['months'];
-    } else {
-        die('Number of Months is empty');
+/*Error output browser*/
+if (!empty($_POST['username']) ) {
+    $username = trim($_POST['username']);
+    if (!isUsernameValid ($username)){
+        $errors[] = 'Username is not valid!';
     }
-    if (is_numeric($_POST['months'])){
-        $months = $_POST['months'];
-    } else {
-        die('Number of months should be indicated as a numerical value');
+}
+
+if (!empty($_POST['taskNumber']) ) {
+    $taskNumber = trim($_POST['taskNumber']);
+    if (!isNaturalNum ($taskNumber)){
+        $errors[] = 'Task Number is not valid!';
     }
+}
+
+if (!empty($_POST['months']) ) {
+    $months = trim($_POST['months']);
+    if (!isNaturalNum ($months)){
+        $errors[] = 'Invalid number of months!';
+    }
+}
+
+if (empty($argv)) {
     include_once 'index.phtml';
 }
 
